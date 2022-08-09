fdce28267ab9049e9ebab4624d17757a5d15f458
Galina Demidova <galinagriz77@gmail.com>
2022-08-09 23:45:15 +0300

    доработка

--- src/test/java/ru/netology/web/FormCardTest.java
+++ src/test/java/ru/netology/web/FormCardTest.java
@@ -33,13 +33,15 @@ public class FormCardTest {
     @Test
     void testFormCartDelivery() {
         $("[data-test-id=city] input").setValue("Краснодар");
-        $("[data-test-id=date] .input__control").setValue(generateDate(12));
+        $("[data-test-id='date'] input").click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] .input__control").setValue(generateDate(6));
         $("[data-test-id=name] .input__control").setValue("Денисов Николай");
         $("[data-test-id=phone] .input__control").setValue("+79894539835");
         $("[data-test-id=agreement]").click();
         $x("//button[contains(@class, 'button_view_extra')]").click();
         $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id=notification] [class='notification__content']").shouldHave(exactText("Встреча успешно забронирована на 12.08.2022"));
+        $("[data-test-id=notification] [class='notification__content']").shouldHave(exactText("Встреча успешно забронирована на 15.08.2022"));
 
     }
 }
